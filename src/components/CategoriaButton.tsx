b9414f244f8d272ebb7b0781219fdbe75e7c866c
mattpsvreis <mateuspsvreis@gmail.com>
2022-07-03 21:23:46 -0300

    Iniciando ProdutoCard e CategoriaButton para poder importá-los

---
+++ src/components/CategoriaButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CategoriaButton = (props: any) => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default CategoriaButton;
---
+++ src/components/ProdutoCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProdutoCard = (props: any) => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default ProdutoCard;
