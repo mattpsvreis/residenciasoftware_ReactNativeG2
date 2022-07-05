29db355673701521370b2aa5813f5340c811b0fe
mattpsvreis <mateuspsvreis@gmail.com>
2022-07-05 01:49:20 -0300

    Página de categoria finalizada

--- src/pages/Categoria/index.tsx
+++ src/pages/Categoria/index.tsx
@@ -1,7 +1,73 @@
 import React from 'react'
+import AxiosInstance from '../../api/AxiosInstance'
+import { AutenticacaoContext } from '../../context/AutenticacaoContext'
 
-export default function Categoria() {
-  return (
-    <></>
-  )
+import { ActivityIndicator, TouchableHighlight, TouchableOpacity, View } from 'react-native'
+import { Icon } from 'react-native-elements'
+import { FlatList } from 'react-native-gesture-handler'
+
+import Text from '../../components/Text'
+
+import ProdutoCard from '../../components/ProdutoCard'
+import ProdutoType from '../../models/ProdutoType'
+
+import styles from './styles'
+
+export default function Categoria({ route, navigation }: any) {
+
+    const { categoria } = route.params;
+    const { usuario } = React.useContext(AutenticacaoContext);
+
+    const [produtos, setProdutos] = React.useState<ProdutoType[]>([]);
+    const [produtosIsLoading, setProdutosIsLoading] = React.useState(true);
+
+    const getDadosProdutos = async () => {
+        AxiosInstance
+            .get('/produto', { headers: { "Authorization": `Bearer ${usuario.token}` } })
+            .then(result => {
+                setProdutos(result.data.filter((prod: any) => prod.idCategoria == categoria.idCategoria));
+                setProdutosIsLoading(false);
+            })
+            .catch((error) => {
+                console.log('Erro ao carregar a lista de produtos: ' + JSON.stringify(error))
+            });
+    }
+
+    const handleReturn = () => {
+        navigation.navigate('HomeTabScreen')
+    }
+
+    React.useEffect(() => {
+        getDadosProdutos();
+    }, [])
+
+    return (
+        <View style={styles.body}>
+            <View style={styles.header}>
+                <TouchableHighlight onPressIn={handleReturn}>
+                    <Icon
+                        name='arrow-left'
+                        color="#dc1e3e"
+                        type='material-community'
+                        size={42}
+                        tvParallaxProperties={undefined}
+                    />
+                </TouchableHighlight>
+                <Text style={styles.title}>{categoria.nomeCategoria}</Text>
+            </View>
+            {produtosIsLoading ?
+                <ActivityIndicator size='large' color='#fff' />
+                :
+                <FlatList
+                    data={produtos}
+                    horizontal={false}
+                    contentContainerStyle={styles.produtosContainer}
+                    renderItem={response => <ProdutoCard produto={response.item} styles={styles} />}
+                    ItemSeparatorComponent={
+                        () => <View style={{ width: 10 }} />
+                    }
+                />
+            }
+        </View>
+    )
 }
---
+++ src/pages/Categoria/styles.tsx
@@ -0,0 +1,57 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    body: {
+        alignItems: 'stretch',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        backgroundColor: '#fff',
+        height: '100%',
+    },
+    header: {
+        flexDirection: 'row',
+        borderBottomWidth: 2,
+        borderBottomColor: '#1119',
+        borderStyle: 'solid',
+        paddingBottom: 9,
+        paddingLeft: 10,
+        width: '100%',
+    },
+    title: {
+        marginLeft: 10,
+        marginTop: 2,
+        textAlign: 'center',
+        fontSize: 28,
+    },
+    produtosContainer: {
+        padding: 0,
+        paddingBottom: 10,
+        marginTop: 10,
+        marginHorizontal: 10,
+    },
+    produtoContainer: {
+        width: 165,
+        height: 229,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    produtoContainerImage: {
+        width: 133,
+        height: 133,
+        borderRadius: 5,
+        marginBottom: 10
+    },
+    textCardTitle: {
+        textAlign: 'left',
+        fontWeight: 'bold',
+        fontSize: 14,
+    },
+    textCardValue: {
+        textAlign: 'left',
+        fontWeight: 'bold',
+        fontSize: 14,
+        color: '#dc1e3e',
+    },
+});
+
+export default styles;
